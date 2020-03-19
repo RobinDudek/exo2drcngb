474b7b5b19dfde09a2bb59c8e06cac56a6309d5e
Noé COlin <noe_colin@yahoo.fr>
2020-03-19 16:36:09 +0100

    make rotate and swipe

--- Assets/Games/Scripts/RotationYScript.cs
+++ Assets/Games/Scripts/RotationYScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RotationYScript : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class RotationYScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0.15f, 0, Space.Self);
     }
 }
---
+++ Assets/Games/Scripts/swpieToRotate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swpieToRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            // GET TOUCH 0
+            Touch touch0 = Input.GetTouch(0);
+
+            // APPLY ROTATION
+            if (touch0.phase == TouchPhase.Moved)
+            {
+                transform.Rotate(0f, touch0.deltaPosition.x, 0f);
+            }
+
+        }
+    }
+}
