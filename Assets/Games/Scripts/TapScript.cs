7168a24a25b13eea2f2497b9e926ccd1e000379c
Bigoud <bigoud33@gmail.com>
2020-03-19 16:34:34 +0100

    Create function to change material of a gameObject

--- Assets/Games/Scripts/TapScript.cs
+++ Assets/Games/Scripts/TapScript.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class TapScript : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public Material[] materials;
+
     void Start()
     {
         
