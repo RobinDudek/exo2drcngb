2bb6f1ffc1d21272a9e3da8861600a4346363890
Bigoud <bigoud33@gmail.com>
2020-03-25 14:57:05 +0100

    Fix fucking mistake (forget ctrl + s)  :'(

--- Assets/Games/Scripts/TapScript.cs
+++ Assets/Games/Scripts/TapScript.cs
@@ -7,15 +7,29 @@ public class TapScript : MonoBehaviour
     // Start is called before the first frame update
 
     public Material[] materials;
+    private Renderer rend;
+    private int cpt;
 
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = materials[cpt];
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void onTap()
+    {
+        cpt++;
+        if (cpt == materials.Length)
+        {
+            cpt = 0;
+        }
+        rend.sharedMaterial = materials[cpt];
     }
 }
